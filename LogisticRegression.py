36b14f16864c73702da0e7e28adc8df6dcf71917
resfeber14 <61600237+resfeber14@users.noreply.github.com>
2020-04-08 20:09:24 +0530

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,3 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing the dataset
+train=pd.read_csv(r"C:\Users\SHWETA KUMARI\Desktop\aa\Titanic\train.csv")
+test=pd.read_csv(r"C:\Users\SHWETA KUMARI\Desktop\aa\Titanic\test.csv")
+X=train.iloc[ : ,[0,2,4,5,6,7]]
+Y=train.iloc[ : , 1:2]
+
+#Missing Values
+X=X.fillna(X.mean())
+
+#Dummy Variables
+X=pd.get_dummies(X)
+
+#Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+col_names=['Pclass','Age','SibSp','Parch']
+features = X[col_names]
+scaler = StandardScaler().fit(features.values)
+features = scaler.transform(features.values)
+X[col_names]= features"""
+
+
 #Fitting the Model
 from sklearn.linear_model import LogisticRegression
 classifier=LogisticRegression()
