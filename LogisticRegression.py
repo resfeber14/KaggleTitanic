393e4b71ebf2b5ca689723c584c875806a228793
resfeber14 <61600237+resfeber14@users.noreply.github.com>
2020-04-08 20:02:43 +0530

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,7 +1,12 @@
 #Fitting the Model
 from sklearn.linear_model import LogisticRegression
 classifier=LogisticRegression()
-classifier.fit(X,Y)
+classifier.fit(X,Y.values.ravel())
+ 
+#Converting test
+test_X=test.iloc[ : ,[0,1,3,4,5,6]]
+test_X=pd.get_dummies(test_X)
+test_X=test_X.fillna(test_X.mean())
 
 #Predicting the Results
-Y_pred=classifier.predict(test)
+Y_pred=classifier.predict(test_X)
